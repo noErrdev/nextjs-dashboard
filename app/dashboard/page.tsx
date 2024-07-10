b9f75f931f934fc4e08b1b9c6709781bc28f3e2c
noErrdev <dbongo0701@gmail.com>
2024-07-10 11:03:26 +0900

    second commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,30 @@
-export default function Page() {
-    return <p>Dashboard</p>
+import { Card } from '@/app/ui/dashboard/cards'
+import RevenueChart from '../ui/dashboard/revenue-chart'
+import LatestInvoices from '../ui/dashboard/latest-invoices'
+import { lusitana } from '../ui/fonts'
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data'
+
+export default async function Page() {
+    const { numberOfInvoices, numberOfCustomers, totalPaidInvoices, TotalPendingInvoices } = await fetchCardData
+
+    const  revenue  = await fetchRevenue();
+    const  latestInvoices = await fetchLatestInvoices();
+    return(
+      <main>
+        <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+            Dashboard
+        </h1>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
+            <Card title="Revenue" value={totalPaidInvoices} type='collected'/>
+            <Card title="Invoices" value={TotalPendingInvoices} type='pending' />
+            <Card title="Total Invoices" value={numberOfInvoices} type='invoices' />
+            <Card title="Total Customers" value={numberOfCustomers} type='customers' />
+        </div>
+        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue}  />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+      </main>
+    )
+    
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
+import { sql } from "@vercel/postgres";
+
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -39,3 +42,4 @@ export default function Page() {
     </main>
   );
 }
+
